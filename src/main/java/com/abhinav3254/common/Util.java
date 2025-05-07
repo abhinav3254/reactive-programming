0875503de673ee6cdaff587ea4164cc699db4734
abhinav3254 <abhinav3254@gmail.com>
2025-05-07 20:45:48 +0530

    common methods

--- src/main/java/com/abhinav3254/common/Util.java
+++ src/main/java/com/abhinav3254/common/Util.java
@@ -1,8 +1,13 @@
 package com.abhinav3254.common;
 
+import com.github.javafaker.Faker;
 import org.reactivestreams.Subscriber;
 
+import java.time.Duration;
+
 public class Util {
+
+    private static final Faker faker = Faker.instance();
 
     public static <T> Subscriber<T> subscriber() {
         return new DefaultSubscriber<>("");
@@ -10,6 +15,18 @@ public class Util {
 
     public static <T> Subscriber<T> subscriber(String name) {
         return new DefaultSubscriber<>(name);
+    }
+
+    public static Faker getFaker() {
+        return faker;
+    }
+
+    public static void sleepSeconds(int seconds) {
+        try {
+            Thread.sleep(Duration.ofSeconds(seconds));
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
