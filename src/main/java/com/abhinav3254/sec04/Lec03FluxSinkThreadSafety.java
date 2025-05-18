d1ffa8c9560f0ada18fe5157c673ce3eb07ad6d6
abhinav3254 <abhinav3254@gmail.com>
2025-05-18 18:36:42 +0530

    array list vs flux sink

--- src/main/java/com/abhinav3254/sec04/Lec03FluxSinkThreadSafety.java
+++ src/main/java/com/abhinav3254/sec04/Lec03FluxSinkThreadSafety.java
@@ -1,17 +1,25 @@
 package com.abhinav3254.sec04;
 
 import com.abhinav3254.common.Util;
+import com.abhinav3254.sec04.helper.NameGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
 
 import java.util.ArrayList;
 
+
+/**
+ * arraylist not thread safe.
+ * flux sink is thread safe
+ */
 public class Lec03FluxSinkThreadSafety {
 
     public static final Logger logger = LoggerFactory.getLogger(Lec03FluxSinkThreadSafety.class);
 
     public static void main(String[] args) {
-        demo();
+//        demo();
+        demo2();
     }
 
     private static void demo() {
@@ -29,4 +37,26 @@ public class Lec03FluxSinkThreadSafety {
         Util.sleepSeconds(5);
         logger.info("size of this list {}",list.size());
     }
+
+    private static void demo2() {
+        // this is thread safe
+        var list = new ArrayList<String>();
+        var generator = new NameGenerator();
+        var flux = Flux.create(generator);
+        flux.subscribe(list::add);
+
+        Runnable runnable = () -> {
+            for (int i = 0; i < 1000; i++) {
+                generator.generate();
+            }
+        };
+
+        for (int i = 0; i < 10; i++) {
+            Thread.ofPlatform().start(runnable);
+        }
+        Util.sleepSeconds(3);
+        logger.info("size of this list {}",list.size());
+
+    }
+
 }
